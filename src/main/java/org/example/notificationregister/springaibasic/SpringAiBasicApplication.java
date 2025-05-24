a69bb96b317f2452cc7b2713bed6c1bf486af9b5
limwoobin <drogba02@naver.com>
2025-05-24 14:43:28 +0900

    feat: Spring AI Basic Project Init

---
+++ src/main/java/org/example/notificationregister/springaibasic/SpringAiBasicApplication.java
@@ -0,0 +1,13 @@
+package org.example.notificationregister.springaibasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiBasicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAiBasicApplication.class, args);
+  }
+
+}
